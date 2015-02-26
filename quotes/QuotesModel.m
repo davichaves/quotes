7e70a76038403ec8a0e631e4d96bb7961f24a568
Davi Chaves <dchaves@usc.edu>
2015-02-25 23:30:26 -0800

    adding

--- quotes/QuotesModel.m
+++ quotes/QuotesModel.m
@@ -8,19 +8,53 @@
 
 #import "QuotesModel.h"
 
-@implementation QuotesModel
+@implementation QuotesModel {
+    NSUInteger current;
+}
 
 @synthesize quotes;
 
 - (instancetype)init {
     self = [super init];
     if (self) {
-        NSDictionary *myDict = [[NSDictionary alloc] init];
-        NSObject *quote = [NSString stringWithFormat:@"%@", @"test"];
-        [myDict addObserver:quote forKeyPath:@"bill" options:nil context:nil];
-        [quotes addObject:quote];
+        current = 0;
     }
     return self;
+}
+
+- (NSDictionary *) randomQuote {
+    return [self quoteAtIndex:rand()];
+}
+
+- (NSUInteger) numberOfQuotes {
+    return [quotes count];
+}
+
+- (NSDictionary *) quoteAtIndex: (NSUInteger) index {
+    return [quotes objectAtIndex:index];
+}
+
+- (void) removeQuoteAtIndex: (NSUInteger) index {
+    [quotes removeObjectAtIndex:index];
+}
+
+- (void) insertQuote: (NSString *) quote author: (NSString *) author atIndex: (NSUInteger) index {
+    [self insertQuote:[NSDictionary dictionaryWithObject:quote forKey:author] atIndex:index];
+}
+- (void) insertQuote: (NSDictionary *) quote atIndex: (NSUInteger) index {
+    [quotes insertObject:quote atIndex:index];
+}
+- (NSDictionary *) nextQuote {
+    if (current < [self numberOfQuotes]-1) {
+        current += 1;
+    }
+    return [quotes objectAtIndex:current];
+}
+- (NSDictionary *) prevQuote {
+    if (current > 0) {
+        current -= 1;
+    }
+    return [quotes objectAtIndex:current];
 }
 
 @end
